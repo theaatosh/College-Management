edbecd3505c1322a71ae5fdca9feb7e597137953
AneshKarki <aneshkarki07@gmail.com>
2025-02-13 20:17:35 +0545

    dashboard

--- Client/src/Admin/Pages/Admin/Dashboard/Dashboard.jsx
+++ Client/src/Admin/Pages/Admin/Dashboard/Dashboard.jsx
@@ -1,6 +1,184 @@
+import "./Dashboard.css";
+import { MdDelete } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="DasdBoardMainDiv">
+        <div className="dashMainTopic">Notices</div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="DasdBoardMainDiv">
+        <div className="dashMainTopic">Achievements</div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="DasdBoardMainDiv">
+        <div className="dashMainTopic">Events</div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+        <div className="noticeList">
+          <div className="noticeListTitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            magni! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rem, laborum?
+          </div>
+          <div className="noticeListIcons">
+            <div>
+              <MdDelete />
+            </div>
+            <div>
+              <CiEdit />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="DasdBoardMainDiv">
+        <div className="dashMainTopic">Landing Page Images</div>
+
+        <div className="landingPageImagesList">
+          <div className="landingPageImages">
+            <img src="" alt="photo" />
+          </div>
+          <div>
+            <MdDelete />
+          </div>
+        </div>
+        <div className="landingPageImagesList">
+          <div className="landingPageImages">
+            <img src="" alt="photo" />
+          </div>
+          <div>
+            <MdDelete />
+          </div>
+        </div>
+        <div className="landingPageImagesList">
+          <div className="landingPageImages">
+            <img src="" alt="photo" />
+          </div>
+          <div>
+            <MdDelete />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
