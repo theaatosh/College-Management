8034086f994af8b0bd71fd7936dbf6eec5efe25c
AneshKarki <aneshkarki07@gmail.com>
2025-03-20 22:20:24 +0545

    vayo

--- server/routes/events.routes.js
+++ server/routes/events.routes.js
@@ -1,23 +1,35 @@
 const { storeImage } = require("../config/cloudinary");
+
 const {
   createEvent,
   fetchEvent,
   deleteEvent,
   updateEvent,
 } = require("../controller/eventsController");
+
 const express = require("express");
 const router = express.Router();
-
-router.get("/fetch", fetchEvent);
+//
+const upload = storeImage("event");
 router.post(
   "/create",
-  (req, res, next) => {
-    console.log("here maw yeta chu");
-    next();
-  },
-  storeImage("event").array("eventPhotos", 5),
+  upload
+    ? upload.array("eventPhotos", 5)
+    : (req, res, next) =>
+        res.status(400).json({ message: "problem in clodinary" }), // Ensure `storeImage` is valid
   createEvent
 );
+//
+router.get("/fetch", fetchEvent);
+// router.post(
+//   "/create",
+//   (req, res, next) => {
+//     console.log("here maw yeta chu");
+//     next();
+//   },
+//   storeImage("event").array("eventPhotos", 5),
+//   createEvent
+// );
 // router.post("/create", (req,res) => {
 //   console.log("here maw yeta chu");
 // });
